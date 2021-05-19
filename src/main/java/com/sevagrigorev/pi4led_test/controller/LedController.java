d9b0d580d99aa6e0b47e1ed6723f45922a5d1080
HowardSTAR <sevagrigoryev@gmail.com>
2021-05-19 16:40:08 +0300

    edit GPIO_19 to 13

--- src/main/java/com/sevagrigorev/pi4led_test/controller/LedController.java
+++ src/main/java/com/sevagrigorev/pi4led_test/controller/LedController.java
@@ -42,7 +42,7 @@ public class LedController {
             System.out.println("OPEN!!!");
             if(pin == null) {
                 GpioController gpio = GpioFactory.getInstance();
-                pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_19, "MyMOTOR", PinState.HIGH); //GPIO_29 && 23
+                pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_13, "MyMOTOR", PinState.HIGH); //GPIO_29 && 23
             }
             pin.toggle();
         }
@@ -50,7 +50,7 @@ public class LedController {
                 System.out.println("CLOSE !!!");
             if(pin == null) {
                 GpioController gpio = GpioFactory.getInstance();
-                pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_19, "MyMOTOR", PinState.LOW);
+                pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_13, "MyMOTOR", PinState.LOW);
             }
             pin.toggle();
             }
