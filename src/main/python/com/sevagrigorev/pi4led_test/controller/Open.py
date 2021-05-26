bfe04f38a880e33097389b6229f11a1641a551b9
Vsevolod <41287640+HowardSTAR@users.noreply.github.com>
2021-05-26 16:51:00 +0300

    set 12, NOT 13

--- src/main/python/com/sevagrigorev/pi4led_test/controller/Open.py
+++ src/main/python/com/sevagrigorev/pi4led_test/controller/Open.py
@@ -8,8 +8,11 @@ GPIO.setup(servoPIN, GPIO.OUT)
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
 p.start(2.5) # Initialization
 try:
-    p.ChangeDutyCycle(13)
+    p.ChangeDutyCycle(12)
     time.sleep(0.5)
+
+    p.stop()
+    GPIO.cleanup()
 
 except KeyboardInterrupt:
   p.stop()
