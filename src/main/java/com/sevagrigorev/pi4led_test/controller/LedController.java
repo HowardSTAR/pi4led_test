99b482dc15782717a480cdc724b04c78d3837658
Vsevolod <41287640+HowardSTAR@users.noreply.github.com>
2021-05-26 15:31:00 +0300

    add Process pOpen & pClose

--- src/main/java/com/sevagrigorev/pi4led_test/controller/LedController.java
+++ src/main/java/com/sevagrigorev/pi4led_test/controller/LedController.java
@@ -45,43 +45,45 @@ public class LedController {
     @PostMapping("/motor")
     public String motor(@RequestParam String btn_) throws IOException {
 
-
         System.out.println("action = " + btn_);
 
         if (btn_.equals("open")) {
             System.out.println("OPEN!!!");
 
-            String pyOpen = "";
-            String[] cmdOpen = new String[2];
-            cmdOpen[0] = "python";
-            cmdOpen[1] = pyOpen;
-
-            Runtime rt = Runtime.getRuntime();
-            Process pr = rt.exec(cmdOpen);
-
-            BufferedReader bfr = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-            String line = "";
-            while ((line = bfr.readLine()) != null){
-                System.out.println(line);
-            }
+            Process pOpen = Runtime.getRuntime().exec("python ");
+//            String pyOpen = "";
+//            String[] cmdOpen = new String[2];
+//            cmdOpen[0] = "python";
+//            cmdOpen[1] = pyOpen;
+//
+//            Runtime rt = Runtime.getRuntime();
+//            Process pr = rt.exec(cmdOpen);
+//
+//            BufferedReader bfr = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+//            String line = "";
+//            while ((line = bfr.readLine()) != null){
+//                System.out.println(line);
+//            }
         }
         if (btn_.equals("close")) {
-            String pyClose = "";
-            String[] cmdClose = new String[2];
-            cmdClose[0] = "python";
-            cmdClose[1] = pyClose;
+            System.out.println("CLOSE!!!");
 
-            Runtime rt = Runtime.getRuntime();
-            Process pr = rt.exec(cmdClose);
-
-            BufferedReader bfr = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-            String line = "";
-            while ((line = bfr.readLine()) != null){
-                System.out.println(line);
-            }
+            Process pClose = Runtime.getRuntime().exec("python ");
+//            String pyClose = "";
+//            String[] cmdClose = new String[2];
+//            cmdClose[0] = "python";
+//            cmdClose[1] = pyClose;
+//
+//            Runtime rt = Runtime.getRuntime();
+//            Process pr = rt.exec(cmdClose);
+//
+//            BufferedReader bfr = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+//            String line = "";
+//            while ((line = bfr.readLine()) != null){
+//                System.out.println(line);
+//            }
             }
         return "motor";
     }
-
 
 }
